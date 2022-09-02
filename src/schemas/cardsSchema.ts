e9d1e06c679a12398d20a9b2ed7c51bb393a27c8
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 14:21:47 -0300

    feat: add employeeId check

--- src/schemas/cardsSchema.ts
+++ src/schemas/cardsSchema.ts
@@ -3,5 +3,6 @@ import joi from 'joi'
 export const createCardSchema = joi.object({
   cardType: joi.string()
     .valid('groceries', 'restaurants', 'transport', 'education', 'health').required(),
-  ApiKey: joi.string().required()
-})
+  ApiKey: joi.string().required(),
+  employeeId: joi.number().integer().required()
+}).strict()
