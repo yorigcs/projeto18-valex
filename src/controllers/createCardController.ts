367dd3096969e3d633ef78a38888532864e705e2
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 17:51:25 -0300

    feat: add InternalError

--- src/controllers/createCardController.ts
+++ src/controllers/createCardController.ts
@@ -3,7 +3,7 @@ import { ParamsDictionary } from 'express-serve-static-core'
 import schemaHandler from '../utils/schemaHandle'
 import { createCardSchema } from '../schemas'
 import { createCardService } from '../services/createCardService'
-import { NotFound } from '../errors'
+import { NotFound, Unauthorized, InternalError } from '../errors'
 
 interface Card {
   cardType: string
@@ -15,14 +15,16 @@ export const createCardController = (req: Request<ParamsDictionary, any, Card>, 
 
   const schemaValidator = schemaHandler(createCardSchema, { cardType, ApiKey, employeeId })
   if (schemaValidator) {
-    return res.status(422).json({ errors: schemaValidator })
+    return res.status(422).json({ message: schemaValidator })
   }
   createCardService(ApiKey, employeeId)
     .then(() => res.status(201).send('Ok'))
     .catch((err) => {
       if (err instanceof NotFound) {
         return res.status(404).send(err)
+      } else if (err instanceof Unauthorized) {
+        return res.status(401).send(err)
       }
-      res.status(500).json({ message: 'An internal error occurred' })
+      res.status(500).send(new InternalError())
     })
 }
