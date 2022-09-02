dbd2d6f6f4a02978ccd6a396a97623c02a3c1849
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 17:52:05 -0300

    feat: ensure the employee belongs to the company

--- src/services/createCardService.ts
+++ src/services/createCardService.ts
@@ -1,10 +1,13 @@
 import { findByApiKey } from '../repositories/companyRepository'
 import { findById } from '../repositories/employeeRepository'
-import { NotFound } from '../errors'
+import { NotFound, Unauthorized } from '../errors'
 export const createCardService = async (apiKey: string, employeeID: number): Promise<void> => {
   const company = await findByApiKey(apiKey)
   if (!company) throw new NotFound('company')
 
   const employee = await findById(employeeID)
   if (!employee) throw new NotFound('employee')
+
+  const isAllowed = company.id === employee.companyId
+  if (!isAllowed) throw new Unauthorized("This employee doesn't belong to the company")
 }
