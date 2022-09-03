c9c42fe596ba5da83cf2df26299c0cd54ccc8c8c
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-03 19:21:03 -0300

    refactor: change schemaHandler to returns a helper

--- src/schemas/schemaHandler.ts
+++ src/schemas/schemaHandler.ts
@@ -1,9 +1,11 @@
 import joi from 'joi'
+import { unprocessableEntity } from '../helpers/httpHelper'
+import { InvalidArgumentError } from '../errors'
+import { HttpResponse } from '../protocols'
 
-export const schemaHandler = (schema: joi.ObjectSchema<any>, object: object): boolean | string[] => {
+export const schemaHandler = (schema: joi.ObjectSchema<any>, object: object): HttpResponse => {
   const { error } = schema.validate(object, { abortEarly: false })
   if (error) {
-    return error.details.map(detail => detail.message)
+    return unprocessableEntity(new InvalidArgumentError(error.details.map(detail => detail.message).join('')))
   }
-  return false
 }
