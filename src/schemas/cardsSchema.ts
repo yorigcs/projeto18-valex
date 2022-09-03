d57145817230dd6763275e77eeb7b09dd1dca9d9
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 23:18:46 -0300

    fix: typo in restaurant

--- src/schemas/cardsSchema.ts
+++ src/schemas/cardsSchema.ts
@@ -2,7 +2,7 @@ import joi from 'joi'
 
 export const createCardSchema = joi.object({
   cardType: joi.string()
-    .valid('groceries', 'restaurants', 'transport', 'education', 'health').required(),
+    .valid('groceries', 'restaurant', 'transport', 'education', 'health').required(),
   ApiKey: joi.string().required(),
   employeeId: joi.number().integer().required()
 }).strict()
