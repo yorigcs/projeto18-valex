5648e09131de6670826e260eaa33b7c265f7458e
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-03 19:23:16 -0300

    feat: create activateCardSchema

--- src/schemas/cardsSchema.ts
+++ src/schemas/cardsSchema.ts
@@ -6,3 +6,9 @@ export const createCardSchema = joi.object({
   ApiKey: joi.string().required(),
   employeeId: joi.number().integer().required()
 }).strict()
+
+export const activateCardSchema = joi.object({
+  cvc: joi.string().pattern(/^[0-9]*$/).required(),
+  employeeId: joi.number().integer().required(),
+  password: joi.string().pattern(/^[0-9]*$/).required()
+}).strict()
