f9548f5670821bd0ecbb2d22c48fe58cada6f465
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 23:23:11 -0300

    feat: ensure there is employee with a card with same type and insert

--- src/services/createCardService.ts
+++ src/services/createCardService.ts
@@ -1,7 +1,13 @@
 import { findByApiKey } from '../repositories/companyRepository'
 import { findById } from '../repositories/employeeRepository'
-import { NotFound, Unauthorized } from '../errors'
-export const createCardService = async (apiKey: string, employeeID: number): Promise<void> => {
+import { findByTypeAndEmployeeId, TransactionTypes, CardInsertData, insert } from '../repositories/cardRepository'
+import { BadRequest, NotFound, Unauthorized } from '../errors'
+import generator from '../utils/generateRandom'
+import { encrypter } from '../utils/cryptrHandler'
+import { generateExpDate } from '../utils/generateDate'
+import generateName from '../utils/generateName'
+
+export const createCardService = async (apiKey: string, employeeID: number, type: TransactionTypes): Promise<void> => {
   const company = await findByApiKey(apiKey)
   if (!company) throw new NotFound('company')
 
@@ -10,4 +16,21 @@ export const createCardService = async (apiKey: string, employeeID: number): Pro
 
   const isAllowed = company.id === employee.companyId
   if (!isAllowed) throw new Unauthorized("This employee doesn't belong to the company")
+
+  const hasCardtypeRegistered = await findByTypeAndEmployeeId(type, employeeID)
+  if (hasCardtypeRegistered) throw new BadRequest('This employee has already this card type registered')
+
+  const cardInfo: CardInsertData = {
+    number: generator(12),
+    employeeId: employeeID,
+    cardholderName: generateName(employee.fullName),
+    securityCode: encrypter(generator(4)),
+    expirationDate: generateExpDate(),
+    isVirtual: false,
+    isBlocked: false,
+    type
+
+  }
+
+  await insert(cardInfo)
 }
