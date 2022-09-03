a65336cacbe352f194777736fd1ccfbec12291ce
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-03 19:29:40 -0300

    feat: create route /activateCard/:employeeId

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { createCardController } from '../controllers'
+import { createCardController, activateCardController } from '../controllers'
 const routeCards = Router()
 
 routeCards.post('/createCard/:employeeId', createCardController)
 
+routeCards.post('/activateCard/:employeeId', activateCardController)
 export { routeCards }
