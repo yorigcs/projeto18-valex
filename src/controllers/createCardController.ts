a82b9f509a3bb7b220017f9641bffe2c67847f0e
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-02 23:22:10 -0300

    feat: add error BadRequest type

--- src/controllers/createCardController.ts
+++ src/controllers/createCardController.ts
@@ -3,11 +3,13 @@ import { ParamsDictionary } from 'express-serve-static-core'
 import schemaHandler from '../utils/schemaHandle'
 import { createCardSchema } from '../schemas'
 import { createCardService } from '../services/createCardService'
-import { NotFound, Unauthorized, InternalError } from '../errors'
+import { NotFound, Unauthorized, BadRequest, InternalError } from '../errors'
+import { TransactionTypes } from '../repositories/cardRepository'
 
 interface Card {
-  cardType: string
+  cardType: TransactionTypes
 }
+
 export const createCardController = (req: Request<ParamsDictionary, any, Card>, res: Response): Response<any, Record<string, any>> => {
   const { cardType } = req.body
   const ApiKey = req.header('x-api-key')
@@ -17,13 +19,15 @@ export const createCardController = (req: Request<ParamsDictionary, any, Card>, 
   if (schemaValidator) {
     return res.status(422).json({ message: schemaValidator })
   }
-  createCardService(ApiKey, employeeId)
+  createCardService(ApiKey, employeeId, cardType)
     .then(() => res.status(201).send('Ok'))
     .catch((err) => {
       if (err instanceof NotFound) {
         return res.status(404).send(err)
       } else if (err instanceof Unauthorized) {
         return res.status(401).send(err)
+      } else if (err instanceof BadRequest) {
+        return res.status(400).send(err)
       }
       res.status(500).send(new InternalError())
     })
