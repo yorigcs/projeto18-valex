775678c2042bf85e251c420bd241e527e96df12b
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-03 19:23:58 -0300

    refactor: change to returns schemaHandler

--- src/controllers/createCardController.ts
+++ src/controllers/createCardController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { createCardSchema, schemaHandler } from '../schemas'
-import { createCardService } from '../services/createCardService'
+import { createCardService } from '../services'
 import { TransactionTypes } from '../repositories/cardRepository'
 
 interface Card {
@@ -13,9 +13,9 @@ export const createCardController = (req: Request<ParamsDictionary, any, Card>, 
   const ApiKey = req.header('x-api-key')
   const employeeId = parseInt(req.params.employeeId)
 
-  const schemaValidator = schemaHandler(createCardSchema, { cardType, ApiKey, employeeId })
-  if (schemaValidator) {
-    return res.status(422).json({ message: schemaValidator })
+  const schema = schemaHandler(createCardSchema, { cardType, ApiKey, employeeId })
+  if (schema) {
+    return res.status(schema.statusCode).send(schema.body)
   }
   const handleService = async (): Promise<any> => {
     const resp = await createCardService(ApiKey, employeeId, cardType)
