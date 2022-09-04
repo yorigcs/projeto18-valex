81dbd30c3587454e371f5f6c782d7c9be3ac2d14
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-04 20:02:16 -0300

    feat: ensure that provided cvs is valid

--- src/services/cardHistoryService.ts
+++ src/services/cardHistoryService.ts
@@ -1,10 +1,15 @@
 import { InvalidParamError } from '../errors'
-import { notFound, ok, serverError } from '../helpers/httpHelper'
-import { findById } from '../repositories/cardRepository'
+import { notFound, ok, serverError, unauthorized } from '../helpers/httpHelper'
+import { findById as findCardById } from '../repositories/cardRepository'
+import { decrypter } from '../utils/cryptrHandler'
 export const cardHistoryService = async (employeeId: number, cardId: number, cvc: string): Promise<any> => {
   try {
-    const card = await findById(cardId)
+    const card = await findCardById(cardId)
     if (!card) return notFound(new InvalidParamError('This card does not exist'))
+
+    const isValidCVC = decrypter(card.securityCode) === cvc
+    if (!isValidCVC) return unauthorized(new InvalidParamError('The cvc is not valid'))
+
     return ok('okay')
   } catch (err) {
     return serverError()
