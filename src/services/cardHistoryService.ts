88e8ca7ddc7e7fbdc039acf413c6d75bc0c0764b
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-04 19:57:05 -0300

    feat: ensure that card exists

--- src/services/cardHistoryService.ts
+++ src/services/cardHistoryService.ts
@@ -1,7 +1,10 @@
-import { ok, serverError } from '../helpers/httpHelper'
-
+import { InvalidParamError } from '../errors'
+import { notFound, ok, serverError } from '../helpers/httpHelper'
+import { findById } from '../repositories/cardRepository'
 export const cardHistoryService = async (employeeId: number, cardId: number, cvc: string): Promise<any> => {
   try {
+    const card = await findById(cardId)
+    if (!card) return notFound(new InvalidParamError('This card does not exist'))
     return ok('okay')
   } catch (err) {
     return serverError()
