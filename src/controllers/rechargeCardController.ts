84859e7ecefdc80cc4b49221d7c4da052e2be08e
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 11:57:34 -0300

    feat: implements rechargeCardService in rechargeCardController

--- src/controllers/rechargeCardController.ts
+++ src/controllers/rechargeCardController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { rechargeCardSchema, schemaHandler } from '../schemas'
+import { rechargeCardService } from '../services'
 
 export const rechargeCardController = (req: Request, res: Response): any => {
   const ApiKey = req.header('x-api-key')
@@ -12,5 +13,9 @@ export const rechargeCardController = (req: Request, res: Response): any => {
     return res.status(schema.statusCode).send(schema.body)
   }
 
-  res.send('ok')
+  const handleService = async (): Promise<any> => {
+    const resp = await rechargeCardService(ApiKey, employeeId, cardId, rechargeAmount)
+    return res.status(resp.statusCode).send(resp.body)
+  }
+  void handleService()
 }
