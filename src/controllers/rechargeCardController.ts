72e8ca9bbd40d8ff1897ffbfca0da5b00a162977
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 11:50:46 -0300

    feat: ensure params are received with rechargeCardSchema

--- src/controllers/rechargeCardController.ts
+++ src/controllers/rechargeCardController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express'
+import { rechargeCardSchema, schemaHandler } from '../schemas'
 
 export const rechargeCardController = (req: Request, res: Response): any => {
+  const ApiKey = req.header('x-api-key')
+  const employeeId = parseInt(req.params.employeeId)
+  const cardId = parseInt(req.params.cardId)
+  const rechargeAmount = parseInt(req.body.rechargeAmount)
+
+  const schema = schemaHandler(rechargeCardSchema, { ApiKey, employeeId, cardId, rechargeAmount })
+  if (schema) {
+    return res.status(schema.statusCode).send(schema.body)
+  }
+
   res.send('ok')
 }
