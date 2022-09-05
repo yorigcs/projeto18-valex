8b50945bdbcfb136493dc2af70d9e3a290b010b3
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-04 21:00:49 -0300

    feat: returns cardHistory if is valid

--- src/controllers/cardHistoryController.ts
+++ src/controllers/cardHistoryController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { cardHistorySchema, schemaHandler } from '../schemas'
 import { cardHistoryService } from '../services'
-export const cardHistoryController = (req: Request, res: Response): any => {
+export const cardHistoryController = (req: Request, res: Response): Response<any, Record<string, any>> => {
   const cardId = parseInt(req.params.cardId)
   const cvc = req.params.cvc
 
--- src/services/cardHistoryService.ts
+++ src/services/cardHistoryService.ts
@@ -1,8 +1,18 @@
 import { InvalidParamError } from '../errors'
 import { notFound, ok, serverError, unauthorized } from '../helpers/httpHelper'
+import { HttpResponse } from '../protocols'
 import { findById as findCardById } from '../repositories/cardRepository'
+import { Payment, findByCardId as findTransactionsByCardId } from '../repositories/paymentRepository'
+import { Recharge, findByCardId as findRechargesByCardId } from '../repositories/rechargeRepository'
 import { decrypter } from '../utils/cryptrHandler'
-export const cardHistoryService = async (employeeId: number, cardId: number, cvc: string): Promise<any> => {
+import { formatDate } from '../utils/generateDate'
+
+interface History {
+  balance: number
+  transactions: Payment []
+  recharges: Recharge []
+}
+export const cardHistoryService = async (cardId: number, cvc: string): Promise<HttpResponse> => {
   try {
     const card = await findCardById(cardId)
     if (!card) return notFound(new InvalidParamError('This card does not exist'))
@@ -10,7 +20,25 @@ export const cardHistoryService = async (employeeId: number, cardId: number, cvc
     const isValidCVC = decrypter(card.securityCode) === cvc
     if (!isValidCVC) return unauthorized(new InvalidParamError('The cvc is not valid'))
 
-    return ok('okay')
+    let balance = 0
+    const recharges = await findRechargesByCardId(cardId)
+    const transactions = await findTransactionsByCardId(cardId)
+    recharges.forEach(recharges => {
+      balance += recharges.amount
+      recharges.timestamp = formatDate(String(recharges.timestamp))
+    })
+    transactions.forEach(transaction => {
+      balance -= transaction.amount
+      transaction.timestamp = formatDate(String(transaction.timestamp))
+    })
+
+    const cardHistory: History = {
+      balance,
+      transactions,
+      recharges
+    }
+
+    return ok(cardHistory)
   } catch (err) {
     return serverError()
   }
