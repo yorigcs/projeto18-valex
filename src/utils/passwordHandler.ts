5e5aa2c05e6eeea6d01b845d67326f41626806b5
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 01:17:32 -0300

    feat: create compateHashPassword

--- src/utils/passwordHandler.ts
+++ src/utils/passwordHandler.ts
@@ -3,3 +3,7 @@ import bcrypt from 'bcrypt'
 export const createHashPassword = (password: string): string => {
   return bcrypt.hashSync(password, 10)
 }
+
+export const compareHashPassword = (password: string, hashedPassword: string): boolean => {
+  return bcrypt.compareSync(password, hashedPassword)
+}
