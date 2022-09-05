853c2ee3b84a9b5c18dea1aa1478f7a3fb02584d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 01:31:09 -0300

    feat: validate and block/unblock cards

--- src/services/cardActionService.ts
+++ src/services/cardActionService.ts
@@ -1,7 +1,7 @@
 import { InvalidParamError } from '../errors'
-import { notFound, ok, serverError, unauthorized } from '../helpers/httpHelper'
+import { badRequest, notFound, ok, serverError, unauthorized } from '../helpers/httpHelper'
 import { HttpResponse } from '../protocols'
-import { findById } from '../repositories/cardRepository'
+import { findById, update } from '../repositories/cardRepository'
 import { isValidDate } from '../utils/generateDate'
 import { compareHashPassword } from '../utils/passwordHandler'
 export const cardActionService = async (cardId: number, action: string, password: string): Promise<HttpResponse> => {
@@ -14,7 +14,13 @@ export const cardActionService = async (cardId: number, action: string, password
     const isValidPassword = compareHashPassword(password, card.password)
     if (!isValidPassword) return unauthorized(new InvalidParamError('This password is incorrect'))
 
-    return ok('ok')
+    if (card.isBlocked && action === 'block') return badRequest(new InvalidParamError('This card is already blocked'))
+
+    if (!card.isBlocked && action === 'unblock') return badRequest(new InvalidParamError('This card is already unblocked'))
+
+    await update(card.id, { isBlocked: action === 'block' })
+
+    return ok(`The card was ${action}ed`)
   } catch (err) {
     return serverError()
   }
