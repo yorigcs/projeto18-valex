a826d926e1968bb0e95a1f07321abc7800b039ae
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 12:12:47 -0300

    feat: insert recharge if card exists

--- src/services/rechargeCardService.ts
+++ src/services/rechargeCardService.ts
@@ -3,6 +3,8 @@ import { ok, serverError, notFound, unauthorized } from '../helpers/httpHelper'
 import { HttpResponse } from '../protocols'
 import { findByApiKey } from '../repositories/companyRepository'
 import { findById as findEmployeeById } from '../repositories/employeeRepository'
+import { findById as findCardById } from '../repositories/cardRepository'
+import { insert, RechargeInsertData } from '../repositories/rechargeRepository'
 
 export const rechargeCardService = async (apiKey: string, employeeId: number, cardId: number, rechargeAmount: number): Promise<HttpResponse> => {
   try {
@@ -15,6 +17,15 @@ export const rechargeCardService = async (apiKey: string, employeeId: number, ca
     const isAllowed = company.id === employee.companyId
     if (!isAllowed) return unauthorized(new InvalidParamError("This employee doesn't belong to the company"))
 
+    const card = await findCardById(cardId)
+    if (!card) return notFound(new InvalidParamError('This card does not exist'))
+
+    const recharge: RechargeInsertData = {
+      cardId,
+      amount: rechargeAmount
+    }
+
+    await insert(recharge)
     return ok('The recharge was successfull')
   } catch (err) {
     return serverError()
