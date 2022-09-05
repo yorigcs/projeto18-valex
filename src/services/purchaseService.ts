8aa407df23fe003c8b6f0ff426816f1506a315de
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 12:35:48 -0300

    feat: ensure card exists

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -1,8 +1,13 @@
-import { ok, serverError } from '../helpers/httpHelper'
+import { InvalidParamError } from '../errors'
+import { notFound, ok, serverError } from '../helpers/httpHelper'
 import { HttpResponse } from '../protocols'
+import { findById } from '../repositories/cardRepository'
 
 export const purchaseService = async (businessId: number, cardId: number, password: string, purchaseAmount: number): Promise<HttpResponse> => {
   try {
+    const card = await findById(cardId)
+    if (!card) return notFound(new InvalidParamError('This card does not exist'))
+
     return ok('ok')
   } catch (err) {
     return serverError()
