6cb8aa070d342c4b7cb17d7120d24cb127e48ccb
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 01:12:57 -0300

    feat: ensure the card is not expired

--- src/services/cardActionService.ts
+++ src/services/cardActionService.ts
@@ -1,11 +1,14 @@
 import { InvalidParamError } from '../errors'
-import { notFound, ok, serverError } from '../helpers/httpHelper'
+import { notFound, ok, serverError, unauthorized } from '../helpers/httpHelper'
 import { HttpResponse } from '../protocols'
 import { findById } from '../repositories/cardRepository'
+import { isValidDate } from '../utils/generateDate'
 export const cardActionService = async (cardId: number, action: string, password: string): Promise<HttpResponse> => {
   try {
     const card = await findById(cardId)
     if (!card) return notFound(new InvalidParamError('This card does not exist'))
+
+    if (!isValidDate(card.expirationDate)) return unauthorized(new InvalidParamError('This card expired'))
 
     return ok('ok')
   } catch (err) {
