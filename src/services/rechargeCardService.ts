993514b109076cbc5609b533303fd410c6fda529
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-05 12:06:13 -0300

    feat: ensure employee exists and belongs to the company

--- src/services/rechargeCardService.ts
+++ src/services/rechargeCardService.ts
@@ -1,8 +1,20 @@
-import { ok, serverError } from '../helpers/httpHelper'
+import { InvalidParamError } from '../errors'
+import { ok, serverError, notFound, unauthorized } from '../helpers/httpHelper'
 import { HttpResponse } from '../protocols'
+import { findByApiKey } from '../repositories/companyRepository'
+import { findById as findEmployeeById } from '../repositories/employeeRepository'
 
-export const rechargeCardService = async (ApiKey: string, employeeId: number, cardId: number, rechargeAmount: number): Promise<HttpResponse> => {
+export const rechargeCardService = async (apiKey: string, employeeId: number, cardId: number, rechargeAmount: number): Promise<HttpResponse> => {
   try {
+    const company = await findByApiKey(apiKey)
+    if (!company) return notFound(new InvalidParamError('x-api-key'))
+
+    const employee = await findEmployeeById(employeeId)
+    if (!employee) return notFound(new InvalidParamError('employeeId'))
+
+    const isAllowed = company.id === employee.companyId
+    if (!isAllowed) return unauthorized(new InvalidParamError("This employee doesn't belong to the company"))
+
     return ok('The recharge was successfull')
   } catch (err) {
     return serverError()
